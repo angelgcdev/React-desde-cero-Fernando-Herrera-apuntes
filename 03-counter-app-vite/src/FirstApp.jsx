497cd57411189f790a0cc98e2abc9aacf2bf295e
Luis Gutierrez <angelgcdev@gmail.com>
2024-08-08 06:11:40 -0400

    continuar desde 011 Impresion de variables en el HTML

---
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -0,0 +1,8 @@
+export const FirstApp = () => {
+  return (
+    <>
+      <h1>Luis Angel</h1>
+      <p>Soy un subtitulo</p>
+    </>
+  );
+};
---
+++ 03-counter-app-vite/src/HelloWorldApp.jsx
@@ -0,0 +1,14 @@
+// function App() {
+//   // document.createElement.....
+//   return <h1>Hola Mundo !! </h1>;
+// }
+
+// export default App;
+
+// export const HelloWorldApp = () => <h1>Arrow Function</h1>;
+
+export const HelloWorldApp = () => {
+  return <h1>Hello World App</h1>;
+};
+
+//FirstApp <h1>Luis Angel</h1>
