d138aafd0959bd196b19d2dacd7c18ac17f7c6c8
Luis Gutierrez <angelgcdev@gmail.com>
2024-08-10 06:47:08 -0400

    continuar desde video 16 tarea componente CounterApp

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -1,8 +1,27 @@
-export const FirstApp = () => {
+import PropTypes from "prop-types";
+
+export const FirstApp = ({ title, subtitle, name }) => {
+  // console.log(props);
+
   return (
     <>
-      <h1>Luis Angel</h1>
-      <p>Soy un subtitulo</p>
+      <h1>{title}</h1>
+      {/* <code>{JSON.stringify(newMessage)}</code> */}
+      <p>{subtitle}</p>
+      <p>{name}</p>
     </>
   );
+};
+
+//Para validar tipos de datos y que sean obligatorios desde el padre que es main.jsx
+FirstApp.propTypes = {
+  title: PropTypes.string.isRequired,
+  subtitle: PropTypes.string,
+};
+
+//Default props para definir valores por defecto
+FirstApp.defaultProps = {
+  title: "No hay titulo...",
+  subtitle: "No hay subtitulo...",
+  name: "Luis Angel",
 };
